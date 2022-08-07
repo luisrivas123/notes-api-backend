e5eb2ac0cba6814274042ffdc774fd5e8cd3b293
luisrivas123 <luis.rivas@correounivalle.edu.co>
2022-08-06 19:25:42 -0500

    init commit

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const logger = require('./loggerMiddleware')
+
+app.use(cors())
+app.use(express.json())
+
+app.use(logger)
+
+let notes = [
+  {
+    userId: 1,
+    id: 1,
+    title: 'Tutorial 1',
+    body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
+  },
+  {
+    userId: 1,
+    id: 2,
+    title: 'Tutorial 2',
+    body: 'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla'
+  },
+  {
+    userId: 1,
+    id: 3,
+    title: 'Tutorial 3',
+    body: 'et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut'
+  }
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello world</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.post('/api/notes', (request, response) => {
+  const note = request.body
+  // console.log(note)
+
+  const ids = notes.map(note => note.id)
+  const maxId = Math.max(...ids)
+
+  const newNote = {
+    id: maxId + 1,
+    title: note.title,
+    body: note.body,
+    important: typeof note.important !== 'undefined' ? note.important : false
+  }
+
+  notes = notes.concat(newNote)
+
+  response.status(201).json(newNote)
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+  response.json(notes)
+  response.status(204).end()
+})
+
+app.use((request, response) => {
+//   console.log(request.path)
+  response.status(404).json({
+    error: 'Not found'
+  })
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`)
+})
